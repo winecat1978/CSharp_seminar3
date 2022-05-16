f6d312fe50b5b7d44e46a9216c4f2cf899d2552c
Katherine_20 <mob1911494@gmail.com>
2022-05-16 20:35:27 +0300

    написали прогу которая опр четверть точки

---
+++ No1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+int GetQuarter(int x, int y)
+{
+    if(x > 0 && y > 0)
+    {
+       return 1;
+    }
+    else if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    else if (x < 0 && y < 0)
+    {
+          return 3;
+    }
+    else return 4;
+}
+
+int GetNumber(string msg) // функция получает число с клавиатуры + проверка на ввод именно числа и число не ноль
+{
+  while(true) // бесконечный цикл 
+   {
+     Console.WriteLine(msg);
+     string valueFromConsole = Console.ReadLine();
+
+       if (int.TryParse(valueFromConsole, out int number)) // преобразовали одну переменную в другую 
+      {
+        if (number != 0) // не равно 
+            return number;
+        else
+        {
+            Console.WriteLine("Число должно отличаться от нуля!");
+        }
+      } 
+       else 
+       {
+           Console.WriteLine("Вы ввели не число. Введите число, отличное от нуля.");
+       }
+    }
+}
+string messageX = "Введите число х: ";
+string messageY = "Введите число y: ";
+
+int x = GetNumber(messageX);
+int y = GetNumber(messageY);
+
+int quater = GetQuarter(x,y);
+
+Console.WriteLine($"точка лежит в {quater} четверти ");
