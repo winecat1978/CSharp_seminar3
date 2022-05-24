1067fbea33c633fc63f9a37340396b1d3ff76463
Katherine_20 <mob1911494@gmail.com>
2022-05-24 11:11:45 +0300

    wrote programm that counts numbers in one number

---
+++ No26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите прогу, которая выдает число цифр в числе
+
+string GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string ValueFromConsole = Console.ReadLine();
+        if (int.TryParse (ValueFromConsole, out int num))
+        {
+            return ValueFromConsole;
+        }
+        else 
+        {
+            Console.WriteLine("Вы ввели не число. Попробуйте еще раз!");
+        }
+    }
+}
+
+int CountNum (string str)
+{
+  return str.Length;
+}
+
+
+string message = "Bведите число: ";
+string str = GetNumber(message);
+int countnum = CountNum(str);
+Console.WriteLine($"{str} -> {countnum}");
