353bd710820ed72001117bccfb6b72b1ced3d3c4
Katherine_20 <mob1911494@gmail.com>
2022-05-24 11:37:52 +0300

    прога выдает рандомный массив из восьми 0 и 1

---
+++ No30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+void FillArray(int[] collection) // функция, заполняющая массив
+{
+    Random Randomiser = new Random(); // объявили класс рандом до цикла
+    for(int i=0; i<collection.Length; i++)
+    {
+        collection[i] = Randomiser.Next(0,2);
+    }
+}
+
+int[] array = new int[8]; // задали массив и его длину
+FillArray(array); // вызвали функцию
+for(int i=0; i<array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
