967bde24be635b4fd12522fcf89b05cf65a0e9b4
Katherine_20 <mob1911494@gmail.com>
2022-05-24 10:51:13 +0300

    программа считает сумму чисел от 1 до N

---
+++ No24/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28 (1+2+3+4+5+6+7)
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string ValueFromConsole = Console.ReadLine();
+        if (int.TryParse(ValueFromConsole, out int num))
+        {
+            if (num > 0)
+            {
+                return num;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не натуральное число. Попробуйте еще раз!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Попробуйте еще раз!");
+        }
+    }
+}
+
+int SummaryOfNumbers(int A)
+{
+    int count = 1;
+    int result = 0;
+    do
+    {
+        result = result + count;
+        count++;
+    }
+    while (A >= count);
+    return result;
+}
+
+string message = "Введите натуральное число.";
+int A = GetNumber(message);
+int Summ = SummaryOfNumbers(A);
+Console.WriteLine($"{A} -> {Summ}");
